c2f90e9ed9cacd35b17f0e8d01eaf22e64d393c2
AlvaroAPB <alvaroparraboccoli@gmail.com>
2026-09-11 12:32:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  *   4. Elimina automáticamente cachés antiguas.
  */
 
-const CACHE_VERSION = "monitoreo-v25b-1";
+const CACHE_VERSION = "monitoreo-v25b-2";
 const APP_SHELL = [
   "./",
   "./index.html",
